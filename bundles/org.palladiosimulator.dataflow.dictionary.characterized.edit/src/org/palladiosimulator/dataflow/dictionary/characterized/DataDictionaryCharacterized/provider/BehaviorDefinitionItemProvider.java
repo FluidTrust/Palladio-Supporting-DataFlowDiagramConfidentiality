f36100542ba36a1f9710d00cc1f6d841095c372a
Stephan Seifermann <stephan.seifermann@kit.edu>
2020-09-27 17:00:25 +0200

    Fixed bug with icons of pins.

--- bundles/org.palladiosimulator.dataflow.dictionary.characterized.edit/src/org/palladiosimulator/dataflow/dictionary/characterized/DataDictionaryCharacterized/provider/BehaviorDefinitionItemProvider.java
+++ bundles/org.palladiosimulator.dataflow.dictionary.characterized.edit/src/org/palladiosimulator/dataflow/dictionary/characterized/DataDictionaryCharacterized/provider/BehaviorDefinitionItemProvider.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Collection;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.palladiosimulator.dataflow.dictionary.characterized.DataDictionaryCharacterized.DataDictionaryCharacterizedPackage;
 
@@ -18,9 +19,18 @@ public class BehaviorDefinitionItemProvider extends BehaviorDefinitionItemProvid
 
     @Override
     public Object getCreateChildImage(Object owner, Object feature, Object child, Collection<?> selection) {
-        if (Arrays.asList(DataDictionaryCharacterizedPackage.Literals.BEHAVIOR_DEFINITION__INPUTS, feature == DataDictionaryCharacterizedPackage.Literals.BEHAVIOR_DEFINITION__OUTPUTS).contains(feature)) {
-            var reference = (EReference)feature;
-            String name = "full/ctool16/Create" + reference.getEContainingClass().getName() + "_" + reference.getName() + ".png";
+        if (Arrays
+            .asList(DataDictionaryCharacterizedPackage.Literals.BEHAVIOR_DEFINITION__INPUTS,
+                    DataDictionaryCharacterizedPackage.Literals.BEHAVIOR_DEFINITION__OUTPUTS)
+            .contains(feature)) {
+            var reference = (EReference) feature;
+            String name = "full/ctool16/Create" + reference.getEContainingClass()
+                .getName() + "_" + reference.getName();
+            if (child instanceof EObject) {
+                name += "_" + ((EObject) child).eClass()
+                    .getName();
+            }
+            name += ".png";
             return getResourceLocator().getImage(name);
         }
         return super.getCreateChildImage(owner, feature, child, selection);
